52d8cae79f8d68e6e3aa8d217701812c5f66fe49
Giovanni Pellegrini <giovannipellegrini@MacBook-Air-di-Giovanni.local>
2026-01-15 11:12:01 +0100

    Codice funzionante ma lento

---
+++ Omega.py
@@ -0,0 +1,167 @@
+from transfer_function import analytical_kernel_RD, kernel_eMD_resonant, kernel_eMD_large_v
+import numpy as np
+from scipy import integrate
+from scipy.special import erfc
+
+"""
+    This section computes the Omega_GW using the analytical kernel for radiation domination and early matter domination.
+"""
+
+
+
+def tranform_to_uv(s,t):
+    #Transform variables from (s,t) to (u,v)
+    u=(t+s+1)/2
+    v=(t-s+1)/2
+    return u,v
+
+
+
+def P_k_lognormal(k, k_p, sigma, A):
+    """
+    Log-normal power spectrum
+     P(k) = A / (sqrt(2*pi) * sigma) * exp(- (log(k/k_p))^2 / (2*sigma^2))
+    paameters:
+    k : Wavenumber
+    k_p : Peak wavenumber
+    sigma : Width of the log-normal distribution
+    A : Amplitude of the power spectrum
+    """
+
+    log_ratio = np.log(k / k_p)
+    normalization = 1.0 / (np.sqrt(2 * np.pi) * sigma)
+    exponential = np.exp(-log_ratio**2 / (2 * sigma**2))
+    
+    return A* normalization * exponential
+
+
+def integrand_RD(s,t,k, P_func,cs_value):
+    """Integrand for Omega_GW computation using analytical kernel in RD."""
+    u,v=tranform_to_uv(s,t)
+    prefactor=(t * (t + 2) * (1 - s**2)/((1 + t - s) * (1 + t + s)))**2
+    I=analytical_kernel_RD(u,v,x_val=1,cs_value=cs_value)
+
+    k1=k * (1 + t + s)/2
+    k2=k * (1 + t - s)/2
+
+    P_ku=P_func(k1)
+    P_kv=P_func(k2)
+
+    return 4*prefactor * I * P_ku * P_kv
+
+
+
+def compute_Omega_RD(k, cs_value, P_func):
+
+    result, error = integrate.dblquad(
+        lambda t, s: integrand_RD(s, t, k, P_func, cs_value),
+        0, 1,                  
+        0, np.inf,  
+    )
+    return result, error
+        
+
+def compute_Omega_RD_today(k, cs_value, P_func, Omega_r0_hh,c_g):
+    """ Compute Omega_GW today during radiation domination phase
+    new parameters:
+    Omega_r0_hh : Radiation density parameter today times h^2
+    c_g : Transfer function factor
+    """
+    Omega_RD, error= compute_Omega_RD(k, cs_value, P_func)
+    Omega_GW = (1/24) * Omega_RD
+    Omega_GW_today= Omega_GW*Omega_r0_hh*c_g
+    return Omega_GW_today, error
+
+
+""" 
+    Functions for computing Omega_GW during early matter domination (eMD) phase.
+"""
+
++
+
+def P_theta(k, k_cut, A_s):
+    if k < k_cut:
+        return A_s
+    else:
+        return 0.0
+    
+
+def integrand_eMD_resonant(s,t,k, eta_R, P_func, Y=2.3):
+    """Integrand for Omega_GW computation using eMD resonant kernel."""
+    
+    if (1 + t - s) <= 0 or (1 + t + s) <= 0 or t <= 0:
+        return 0.0
+    prefactor=(t * (t + 2) * (1 - s**2)/((1 + t - s) * (1 + t + s)))**2
+    I=kernel_eMD_resonant(s, t, k, eta_R, Y)
+
+    k1=k * (1 + t + s)/2
+    k2=k * (1 + t - s)/2
+
+    P_ku=P_func(k1)
+    P_kv=P_func(k2)
+
+    return 4* I * P_ku * P_kv * prefactor
+
+def integrand_eMD_large_v(s,t,k,k_max,eta_R, P_func):
+    """Integrand for Omega_GW computation using eMD large v kernel."""
+    if (1 + t - s) <= 0 or (1 + t + s) <= 0 or t <= 0:
+        return 0.0
+    
+    prefactor=(t * (t + 2) * (1 - s**2)/((1 + t - s) * (1 + t + s)))**2
+    I=kernel_eMD_large_v(s, t, k, eta_R)
+    k1=k * (1 + t + s)/2
+    k2=k * (1 + t - s)/2
+
+    P_ku=P_func(k1)
+    P_kv=P_func(k2)
+
+    return 4* I * P_ku * P_kv*prefactor
+
+def compute_Omega_eMD_resonant(k, eta_R, k_max, P_func, Y=2.3):
+    k_thr = 2*k_max/np.sqrt(3)
+    if k > k_thr:
+        return 0.0, 0.0
+    limit_t_upper = lambda s: -s + 2*k_max/k - 1
+    limit_t_lower = lambda s: 0  
+    result, error = integrate.dblquad(
+        lambda t, s: integrand_eMD_resonant(s, t, k, eta_R, P_func, Y),
+        0, 1,     # limiti per s
+        limit_t_lower,          # Limite inferiore interno (t)
+        limit_t_upper,         # Limite superiore interno (t)
+        epsabs=1e-12,epsrel=1e-6
+    )
+    return result, error
+
+def compute_Omega_eMD_large_v(k, eta_R, k_max, P_func):
+    
+    limit_t_upper = lambda s: (-s + 2*k_max/k - 1)
+    limit_t_lower = lambda s: 0  
+    result, error = integrate.dblquad(
+        lambda t, s: integrand_eMD_large_v(s, t, k, k_max, eta_R,P_func),
+        0, 1,                   # Limiti esterni (s)
+        limit_t_lower,          # Limite inferiore interno (t)
+        limit_t_upper,         # limiti per s
+    )
+    return result, error
+
+def compute_Omega_eMD_total(k, eta_R, k_max, P_func, Y=2.3):
+    omega_resonant, error_resonant = compute_Omega_eMD_resonant(k, eta_R, k_max, P_func, Y)
+    omega_large_v, error_large_v = compute_Omega_eMD_large_v(k, eta_R, k_max, P_func)
+    
+    total_omega = omega_resonant + omega_large_v
+    total_error= np.sqrt(error_resonant**2 + error_large_v**2)
+
+    return total_omega, total_error
+
+def compute_Omega_eMD_today(k, eta_R, k_max, P_func, Omega_r0_hh,c_g):
+    """ Compute Omega_GW today during radiation domination phase after eMD with sudden reheating.
+    new parameters:
+    Omega_r0_hh : Radiation density parameter today times h^2
+    c_g : Transfer function factor
+    """
+    x_R= k * eta_R
+    Omega_GW, error= compute_Omega_eMD_total(k, eta_R, k_max, P_func)
+    Omega_GW = (1/24) * x_R**2 * Omega_GW
+    Omega_GW_today= Omega_GW*Omega_r0_hh*c_g
+    return Omega_GW_today, error
+
---
+++ transfer_function.py
@@ -0,0 +1,111 @@
+import sympy as sp
+import numpy as np
+from scipy import integrate
+from scipy.special import sici
+
+
+"""
+    Kernel functions for radiation domination (RD) phase.
+"""
+cs,u,v,x,xb,z=sp.symbols('cs u v x xb z', real=True, positive=True)
+
+def make_kernel_RD(cs_value=1/np.sqrt(3)):
+    
+
+    def T_RD(x):
+        return (9/(x**2))*(sp.sin(cs*x)/(cs*x) - sp.cos(cs*x))    
+
+    def build_transfer_function(u,v,x):
+
+        Tz=T_RD(z)
+        dTz=sp.diff(Tz, z)
+
+        U=u*xb
+        V=v*xb
+
+        Tu=Tz.subs(z, U)
+        Tv=Tz.subs(z, V)
+        dT_u=dTz.subs(z, U)
+        dT_v=dTz.subs(z, V)
+
+        f=sp.Rational(1,9)*(12*Tu*Tv +4*(xb**2)*u*v*dT_u*dT_v +4*xb*(u*Tv*dT_u + v*Tu*dT_v))
+        return f
+
+
+    def green_function(u,v,x):
+        return sp.sin(x-xb)
+
+    integrand_expr = xb*build_transfer_function(u,v,x) * green_function(u,v,x)
+    integrand_expr = integrand_expr.subs(cs, cs_value)
+    integrand_np=sp.lambdify((u,v,x,xb), integrand_expr, "numpy")
+
+    def I(u_val, v_val, x_val, **quad_kwargs):
+        val, err = integrate.quad(
+            lambda xb_val: integrand_np(u_val, v_val, x_val, xb_val),
+            0.0, float(x_val),
+            **quad_kwargs
+        )
+        return val/x_val
+
+    return I
+
+
+def analytical_kernel_RD(u_val, v_val, x_val=1, cs_value=1/np.sqrt(3)):
+    
+    prefactor = 1/2 * np.pow((3 * (u_val**2 + v_val**2 - 3)) / (4 * u_val**3 * v_val**3 * x_val), 2)
+    
+    # Second term
+    num = 3 - (u_val + v_val)**2
+    den = 3 - (u_val - v_val)**2
+    log_arg = np.abs(num / den)
+    term_log = (u_val**2 + v_val**2 - 3) * np.log(log_arg)
+    second_term = -4 * u_val * v_val + term_log 
+    
+    # Third term
+    theta = 1 if (u_val + v_val) >= 1/cs_value else 0
+    third_term = theta * np.pi**2 * (u_val**2 + v_val**2 - 3)**2
+    
+    result = prefactor * (second_term**2 + third_term)
+    return result
+
+
+"""
+Kernel functions for early matter domination (eMD) phase.
+"""
+
+
+def kernel_eMD_resonant(s,t,k,eta_R, Y=2.3):
+    """Kernel for early matter domination at resonance.
+        Parameters:
+        t : Integration variable
+        s : Integration variable
+        k : Wavenumber
+        eta_R : Conformal time at reathing
+        Y : Numerical factor (default is 2.3)
+        """
+
+    x_R= k * eta_R
+    y=np.abs(t+1-np.sqrt(3)) * x_R / (2 * np.sqrt(3))
+    _, ci_y= sici(y)
+    numerator=Y * 9 * (-5 + s**2 + 2*t + t**2)**4 * x_R**8
+    denominator=81920000 * (1 - s + t)**2 * (1 + s + t)**2
+
+    return numerator / denominator * ci_y**2
+
+def kernel_eMD_large_v(s,t,k,eta_R):
+    """Kernel for early matter domination at large wavelength.
+        Parameters:
+        t : Integration variable
+        s : Integration variable
+        k : Wavenumber
+        eta_R : Conformal time at reathing
+        """
+
+    x_R= k * eta_R
+    Si_half, Ci_half= sici(x_R / 2)
+
+    numerator= 9* t**4 * x_R**8 * (4 *Ci_half**2 + (np.pi - 2 * Si_half)**2 )
+    denominator=2**17 * 5**4
+
+    return numerator / denominator
+
